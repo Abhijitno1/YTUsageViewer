1e46cfa50a20a2ec7ced708f764b47584c77d359
Abhijit Desai <abhijitno1@yahoo.com>
2021-12-05 16:57:04 -0500

    Contacts upload functionality implemented

---
+++ Controllers/BulkUploadController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YTUsageViewer.Helpers;
+
+namespace YTUsageViewer.Controllers
+{
+    public class BulkUploadController : Controller
+    {
+        public ActionResult Index()
+        {
+            return PartialView();
+        }
+
+        //ToDo: Convert to async method
+        public ActionResult UploadCSV(HttpPostedFileBase fileUpload)
+        {
+            try
+            {
+                if (fileUpload != null)
+                {
+                    //fileUpload.SaveAs(Path.Combine(Server.MapPath("~/Assets/"), fileUpload.FileName));
+                    if (!fileUpload.FileName.ToLower().EndsWith(".csv"))
+                    {
+                        ModelState.AddModelError("fileUpload", "Please select a CSV file to upload");
+                        return PartialView("Index");
+                    }
+                    CsvImporter importer = new CsvImporter();
+                    DataTable newContacts = importer.GetDataTableFromCSVFile(fileUpload.InputStream);
+                    importer.SaveContacts2DB(newContacts);
+                    //return RedirectToAction("Index", "Movie");
+                    var response = new { Success = true, Message = "Contacts uploaded successfully" };
+                    return Json(response);
+                }
+                else
+                {
+                    ModelState.AddModelError("fileUpload", "Please select a file to upload");
+                    return PartialView("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("fileUpload", ex);
+                return PartialView("Index");
+            }
+        }
+    }
+}
---
+++ Helpers/CsvImporter.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web;
+using YTUsageViewer.Models;
+
+namespace YTUsageViewer.Helpers
+{
+    public class CsvImporter
+    {
+        private ApplicationDbContext dbContext = new ApplicationDbContext();
+
+        public DataTable GetDataTableFromCSVFile(Stream inputStream, bool hasFieldsEnclosedInQuotes = false)
+        {
+            var csvData = new DataTable();
+            try
+            {
+                using (TextFieldParser csvReader = new TextFieldParser(inputStream))
+                {
+                    csvReader.SetDelimiters(new string[] { "," });
+                    csvReader.HasFieldsEnclosedInQuotes = hasFieldsEnclosedInQuotes;
+                    string[] colFields = csvReader.ReadFields();
+                    foreach (string column in colFields)
+                    {
+                        DataColumn datecolumn = new DataColumn(column);
+                        datecolumn.AllowDBNull = true;
+                        csvData.Columns.Add(datecolumn);
+                    }
+
+                    while (!csvReader.EndOfData)
+                    {
+                        string[] fieldData = csvReader.ReadFields();
+                        csvData.Rows.Add(fieldData);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception:{0}", ex.Message);
+            }
+            return csvData;
+        }
+
+        public void SaveContacts2DB(DataTable contacts)
+        {
+            var columnExists = new Dictionary<string, bool>() 
+            {
+                {"FirstName", contacts.Columns.Contains("FirstName") },
+                {"LastName", contacts.Columns.Contains("LastName") },
+                {"PhoneHome", contacts.Columns.Contains("PhoneHome") },
+                {"PhoneMobile", contacts.Columns.Contains("PhoneMobile") },
+                {"PhoneWork", contacts.Columns.Contains("PhoneWork") },
+                {"PreferredPhone", contacts.Columns.Contains("PreferredPhone") },
+                {"Email", contacts.Columns.Contains("Email") }
+            };
+
+            foreach (DataRow dataRow in contacts.Rows)
+            {
+                var newContact = new Contact();
+                if (columnExists["FirstName"])
+                    newContact.FirstName = Convert.ToString(dataRow["FirstName"]);
+                if (columnExists["LastName"])
+                    newContact.LastName = Convert.ToString(dataRow["LastName"]);
+                if (columnExists["PhoneHome"])
+                    newContact.PhoneHome = Convert.ToString(dataRow["PhoneHome"]);
+                if (columnExists["PhoneMobile"])
+                    newContact.PhoneMobile = Convert.ToString(dataRow["PhoneMobile"]);
+                if (columnExists["PhoneWork"])
+                    newContact.PhoneWork = Convert.ToString(dataRow["PhoneWork"]);
+                if (columnExists["PreferredPhone"])
+                    newContact.PreferredPhone = Convert.ToString(dataRow["PreferredPhone"]);
+                if (columnExists["Email"])
+                    newContact.Email = Convert.ToString(dataRow["Email"]);
+
+                dbContext.Contacts.Add(newContact);
+            }
+            dbContext.SaveChanges();
+        }
+    }
+}
